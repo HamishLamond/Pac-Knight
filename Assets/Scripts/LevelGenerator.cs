19c5ced495630832063ece5634bfdbe2361fd945
HamishLamond <62045373+HamishLamond@users.noreply.github.com>
2020-09-14 13:49:46 +1000

    Prefab Creation
    Created initial prefabs for walls. Also created LevelGenerator Script

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject insideWall;
+    [SerializeField]
+    private GameObject insideCorner;
+    [SerializeField]
+    private GameObject outsideWall;
+    [SerializeField]
+    private GameObject outsideCorner;
+    [SerializeField]
+    private GameObject tJunction;
+    [SerializeField]
+    private GameObject sword;
+    [SerializeField]
+    private GameObject armour;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        generateLevel();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void generateLevel()
+    {
+
+    }
+}
