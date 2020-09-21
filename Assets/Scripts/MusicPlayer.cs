546ec963a017349cf318be467cf4135f2503fd7e
HamishLamond <62045373+HamishLamond@users.noreply.github.com>
2020-09-21 12:39:34 +1000

    Added Music to scene
    Added audio source and appropriate music to it. Also included different cuts of songs for the game.

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    public AudioClip[] songs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<AudioSource>().clip = songs[0];
+        GetComponent<AudioSource>().Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GetComponent<AudioSource>().isPlaying == false)
+        {
+            GetComponent<AudioSource>().clip = songs[1];
+            GetComponent<AudioSource>().Play();
+        }
+    }
+}
